b572523b7c51e80ba9953bc0f61cece5f6709f3f
DevCode-C <emmanuelparedescamargo@gmail.com>
2021-05-21 01:04:01 -0500

    First commit

---
+++ app/funciones.h
@@ -0,0 +1,22 @@
+#ifndef FUNCIONES__H__
+    #define FUNCIONES__H__
+
+    #define TASK 4
+
+    void initTask(void);
+    #if (TASK == 1)
+        void task(void);
+    #endif
+
+    #if (TASK == 2)
+        void task(void);
+    #endif
+
+    #if (TASK == 3)
+        void task(void);
+    #endif
+
+    #if (TASK == 4)
+        void task(void);
+    #endif
+#endif
